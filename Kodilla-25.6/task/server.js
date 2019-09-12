c497305d4f3e04a81905490dd9e8be4809c70197
Bartek.B <bartek.bugala@gmail.com>
2019-09-12 16:06:12 +0200

    fixes after review change to express.static for style.css remove comment fix formmating remove bad endpoint

--- Kodilla-25.6/task/server.js
+++ Kodilla-25.6/task/server.js
@@ -4,27 +4,19 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/vendor/bootstrap.min.css', function (req, res) {
-    res.sendFile('./vendor/bootstrap.min.css', { root: __dirname });
-});
-app.get('/style.css', function (req, res) {
-    res.sendFile('./style.css', { root: __dirname });
+app.use(express.static('assets'));
+
+app.get('/auth/google', function(req, res) {
+  res.render('dynamic-user', {
+    user: { email: 'user@gmail.com', password: 'pass' }
+  });
 });
 
-app.get('/auth/google', function (req, res) {
-    res.render('dynamic-user',
-        {
-            user:
-                { email: 'user@gmail.com', password: 'pass' }
-        }
-    );
-});
-
-app.get('/', function (req, res) {
-    res.render('glogin');
+app.get('/', function(req, res) {
+  res.render('glogin');
 });
 
 app.listen(3000);
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+app.use(function(req, res, next) {
+  res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
 });
